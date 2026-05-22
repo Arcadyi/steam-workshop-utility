a3114e8ac459374544ad85dca8016abd579c93c9
Arcadyi <denizhandeasis@gmail.com>
2026-05-22 16:38:48 +0300

    fix: limit window size on windows to prevent ui bugs

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,18 @@ pub mod types;
 pub mod utils;
 pub mod icons;
 fn main() -> cosmic::iced::Result {
+    let max_width = if cfg!(target_os = "windows") { 1280.0 } else { f32::INFINITY };
+    let max_height = if cfg!(target_os = "windows") { 800.0 } else { f32::INFINITY };
+
     let settings = cosmic::app::Settings::default()
         .size(cosmic::iced::Size::new(1280.0, 800.0))
         .size_limits(
             cosmic::iced::Limits::NONE
-                .min_width(500.0)
-                .min_height(300.0)
+                .min_width(1280.0)
+                .min_height(800.0)
+                .max_width(max_width)
+                .max_height(max_height),
         );
+
     cosmic::app::run::<app::AppModel>(settings, ())
 }

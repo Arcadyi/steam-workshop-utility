24136b9fff5f37b9606af66c5822e38fe89c6f16
Arcadyi <denizhandeasis@gmail.com>
2026-05-23 12:31:49 +0300

    fix: window size on windows

--- src/main.rs
+++ src/main.rs
@@ -7,14 +7,14 @@ pub mod utils;
 pub mod icons;
 fn main() -> cosmic::iced::Result {
     let max_width = if cfg!(target_os = "windows") { 1280.0 } else { f32::INFINITY };
-    let max_height = if cfg!(target_os = "windows") { 800.0 } else { f32::INFINITY };
+    let max_height = if cfg!(target_os = "windows") { 900.0 } else { f32::INFINITY };
 
     let settings = cosmic::app::Settings::default()
-        .size(cosmic::iced::Size::new(1280.0, 800.0))
+        .size(cosmic::iced::Size::new(1280.0, 900.0))
         .size_limits(
             cosmic::iced::Limits::NONE
                 .min_width(1280.0)
-                .min_height(800.0)
+                .min_height(900.0)
                 .max_width(max_width)
                 .max_height(max_height),
         );
